7367d690283b9b50fd2f5207cabe255f3c494287
ussumane <ussumane.momade@uem.ac.mz>
2024-07-23 17:38:25 +0200

    setup all files

---
+++ 0x00-Webpack/task_3/webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './js/dashboard_main.js',
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: 'bundle.js'
+    },
+    // performance: {
+    //     maxAssetSize: 1000000,
+    //     maxEntrypointSize: 1000000,
+    // },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            },
+            {
+                test: /\.(gif|png|jpe?g|svg)$/i,
+                use: [
+                    'file-loader',
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            // bypassOnDebug: true,
+                            // disable: true,
+                            mozjpeg: {
+                                progressive: true,
+                            },
+                            // optipng.enabled: false will disable optipng
+                            optipng: {
+                                enabled: false,
+                            },
+                            pngquant: {
+                                quality: [0.65, 0.90],
+                                speed: 4
+                            },
+                            gifsicle: {
+                                interlaced: false,
+                            },
+                            // the webp option will enable WEBP
+                            webp: {
+                                quality: 75
+                            }
+                        },
+                    },
+                ],
+            }
+        ]
+    }
+}
