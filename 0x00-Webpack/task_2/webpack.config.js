982680bcfcfb3bc4cf0cf0304e7c824da29be45d
Ussumane Momade <89344441+Uss-Momas@users.noreply.github.com>
2024-07-23 17:19:06 +0200

    fix performance

--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -7,10 +7,10 @@ module.exports = {
         path: path.resolve(__dirname, 'public'),
         filename: 'bundle.js'
     },
-    performance: {
-        maxAssetSize: 1000000,
-        maxEntrypointSize: 1000000,
-    },
+    // performance: {
+    //     maxAssetSize: 1000000,
+    //     maxEntrypointSize: 1000000,
+    // },
     module: {
         rules: [
             {
@@ -24,8 +24,26 @@ module.exports = {
                     {
                         loader: 'image-webpack-loader',
                         options: {
-                            bypassOnDebug: true,
-                            disable: true,
+                            // bypassOnDebug: true,
+                            // disable: true,
+                            mozjpeg: {
+                                progressive: true,
+                            },
+                            // optipng.enabled: false will disable optipng
+                            optipng: {
+                                enabled: false,
+                            },
+                            pngquant: {
+                                quality: [0.65, 0.90],
+                                speed: 4
+                            },
+                            gifsicle: {
+                                interlaced: false,
+                            },
+                            // the webp option will enable WEBP
+                            webp: {
+                                quality: 75
+                            }
                         },
                     },
                 ],
