84d74e7595c06abecc22ed66ccf482fca0fb8544
Ussumane Momade <89344441+Uss-Momas@users.noreply.github.com>
2024-07-23 14:40:40 +0200

    creating the output file

--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
     mode: 'production',
-    // entry: './js/dashboard_main.js',
+    entry: './js/dashboard_main.js',
     output: {
         path: path.resolve(__dirname, 'public'),
         filename: 'bundle.js'
