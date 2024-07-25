a356fb43ccf7af4711f44d7802f277eb7e2ebc61
ussumane <ussumane.momade@uem.ac.mz>
2024-07-26 00:55:43 +0200

    setup react with webpack

---
+++ 0x02-react_props/task_0/dashboard/src/Notifications/Notifications.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Notifications.css';
+import { getLatestNotification } from '../utils/utils';
+import closeIcon from '../assets/close-icon.png';
+
+export default function Notifications() {
+  const createLatestNotification = () => { return { __html: getLatestNotification() }; }
+  const handleClick = (event) => {
+    console.log("Close button has been clicked");
+  }
+  return (
+    <div className="Notifications">
+      <div>
+        <p>Here is the list of notifications</p>
+        <ul>
+          <li data-priority="default">New course available</li>
+          <li data-priority="urgent">New resume available</li>
+          <li data-priority="urgent" dangerouslySetInnerHTML={createLatestNotification()}></li>
+        </ul>
+      </div>
+      <button aria-label='Close' onClick={handleClick}>
+        <img src={closeIcon} alt="close-icon.png" />
+      </button>
+    </div>
+  );
+}
