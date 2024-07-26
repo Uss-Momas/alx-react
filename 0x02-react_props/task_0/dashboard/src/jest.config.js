7f5e85429199771477b0ae1736d2e2b48b5a18c3
Ussumane Momade <89344441+Uss-Momas@users.noreply.github.com>
2024-07-26 09:34:41 +0200

    jest configuration

--- 0x02-react_props/task_0/dashboard/src/jest.config.js
+++ 0x02-react_props/task_0/dashboard/src/jest.config.js
@@ -16,17 +16,9 @@ const config = {
   moduleNameMapper: {
     "\\.(css|sass)$": "identity-obj-proxy",
   },
-  // The test environment that will be used for testing
-  testEnvironment: 'jsdom',
-
-  // The glob patterns Jest uses to detect test files
-  testMatch: ['**/__tests__/**/*.js?(x)', '**/?(*.)+(spec|test).js?(x)'],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
   testPathIgnorePatterns: ['\\\\node_modules\\\\'],
-
-  // This option sets the URL for the jsdom environment. It is reflected in properties such as location.href
-  testURL: 'http://localhost',
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   transformIgnorePatterns: ['<rootDir>/node_modules/'],
