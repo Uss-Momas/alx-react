39920e48e13c89aaa9eb52f30f0d13e4f89b1d78
Ussumane Momade <89344441+Uss-Momas@users.noreply.github.com>
2024-07-26 11:57:32 +0200

    add testing

--- 0x02-react_props/task_0/dashboard/src/App/App.test.js
+++ 0x02-react_props/task_0/dashboard/src/App/App.test.js
@@ -7,4 +7,20 @@ describe('<App /> Component Test cases', () => {
     it('Renders app withou crashing', () => {
         expect(wrapper).toBeDefined();
     });
+
+    it('Verify if exists <Notifications/> component', () => {
+        expect(wrapper.find('Notifications').exists()).toEqual(true);
+    });
+
+    it('Verify if exists <Header/> component', () => {
+        expect(wrapper.find('Header').exists()).toEqual(true);
+    });
+
+    it('Verify if exists <Login/> component', () => {
+        expect(wrapper.find('Login').exists()).toEqual(true);
+    });
+
+    it('Verify if exists <Footer/> component', () => {
+        expect(wrapper.find('Footer').exists()).toEqual(true);
+    });
 });
