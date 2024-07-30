de7901cfb0ddad13ae53fde1aa7070677602e33a
Ussumane Momade <89344441+Uss-Momas@users.noreply.github.com>
2024-07-30 12:40:29 +0200

    add tests

---
+++ 0x03-React_component/task_1/dashboard/src/Notifications/Notifications.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import closeIcon from '../assets/close-icon.png';
+import './Notifications.css';
+import { getLatestNotification } from '../utils/utils';
+import NotificationItem from './NotificationItem';
+import PropTypes from 'prop-types';
+
+function Notifications({ displayDrawer = true }) {
+  const handleClick = (event) => {
+    console.log("Close button has been clicked");
+  }
+  return (
+    <>
+      <div className="menuItem">
+        <span className='my-notifications'>Your notifications</span>
+        {displayDrawer && (<div className="Notifications">
+          <div>
+            <p>Here is the list of notifications</p>
+            <ul>
+              <NotificationItem type="default" value="New course available" />
+              <NotificationItem type="urgent" value="New resume available" />
+              <NotificationItem type="urgent" html={{ __html: getLatestNotification() }} />
+            </ul>
+          </div>
+          <button aria-label='Close' onClick={handleClick}>
+            <img src={closeIcon} alt="close-icon.png" />
+          </button>
+        </div>)}
+      </div>
+    </>
+  );
+}
+
+Notifications.propTypes = {
+  displayDrawer: PropTypes.bool,
+}
+
+export default Notifications;
