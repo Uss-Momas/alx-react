0a0f5da1ee8d99e4b657de759c77ef9d44c509b2
Ussumane Momade <89344441+Uss-Momas@users.noreply.github.com>
2024-07-31 12:59:44 +0200

    create body section components and use them

---
+++ 0x03-React_component/task_3/dashboard/src/App/App.js
@@ -0,0 +1,84 @@
+import './App.css';
+import BodySectionWithMarginBottom from '../BodySection/BodySectionWithMarginBottom';
+import { Component } from "react";
+import CourseList from '../CourseList/CourseList';
+import Footer from '../Footer/Footer';
+import Header from '../Header/Header';
+import Login from '../Login/Login';
+import Notifications from '../Notifications/Notifications';
+import PropTypes from 'prop-types';
+import { getLatestNotification } from '../utils/utils';
+import BodySection from '../BodySection/BodySection';
+
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.handleKeyboardPress = this.handleKeyboardPress.bind(this);
+    }
+
+    handleKeyboardPress(event) {
+        if (event.ctrlKey && event.key === 'h') {
+            alert('Logging you out');
+            this.props.logOut();
+        }
+    }
+
+    componentDidMount() {
+        document.addEventListener('keydown', this.handleKeyboardPress)
+    }
+
+    componentWillUnmount() {
+        document.removeEventListener('keydown', this.handleKeyboardPress)
+    }
+
+    render() {
+
+        const listCourses = [
+            { id: 1, name: 'ES6', credit: 60 },
+            { id: 2, name: 'Webpack', credit: 20 },
+            { id: 3, name: 'React', credit: 40 },
+        ];
+
+        const listNotifications = [
+            { id: 1, type: "default", value: "New course available" },
+            { id: 2, type: "urgent", value: "New resume available" },
+            { id: 3, type: "urgent", html: { __html: getLatestNotification() } },
+        ];
+
+        return <>
+            <div className="App">
+                <div className='headerContainer'>
+                    <Header />
+                    <Notifications listNotifications={listNotifications} />
+                </div>
+                <div className="App-body">
+                    {
+                        !this.props.isLoggedIn ? (<BodySectionWithMarginBottom title='Log in to continue'>
+                            <Login />
+                        </BodySectionWithMarginBottom>) : (
+                            <BodySectionWithMarginBottom title="Course list">
+                                <CourseList listCourses={listCourses} />
+                            </BodySectionWithMarginBottom>
+                        )
+                    }
+                    <BodySection title={'News from the School'}>
+                        <p>Here are the news from school....</p>
+                    </BodySection>
+                </div>
+                <Footer />
+            </div>
+        </>
+    };
+}
+
+App.propTypes = {
+    logOut: PropTypes.func
+}
+
+App.defaultProps = {
+    logOut: () => { return; }
+}
+
+
+export default App;
