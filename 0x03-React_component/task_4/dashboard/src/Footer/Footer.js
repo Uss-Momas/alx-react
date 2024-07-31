1f6839cf8e530211eba86898050fac780623d0fb
Ussumane Momade <89344441+Uss-Momas@users.noreply.github.com>
2024-07-31 14:03:49 +0200

    Create High Order Component

---
+++ 0x03-React_component/task_4/dashboard/src/Footer/Footer.js
@@ -0,0 +1,10 @@
+import './Footer.css';
+import { getFullYear, getFooterCopy } from '../utils/utils';
+
+export default function Footer() {
+    return (
+        <div className="App-footer">
+            <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
+        </div>
+    );
+}
