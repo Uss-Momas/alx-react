e9f1aeecea32be39ee61b3829600a6d45b94b846
Ussumane Momade <89344441+Uss-Momas@users.noreply.github.com>
2024-08-05 11:25:14 +0200

    add inline style

--- 0x04-React_inline_styling/task_0/dashboard/src/CourseList/CourseListRow.js
+++ 0x04-React_inline_styling/task_0/dashboard/src/CourseList/CourseListRow.js
@@ -1,10 +1,12 @@
 export default function CourseListRow({ isHeader, textFirstCell, textSecondCell }) {
+    const rowStyle = { backgroundColor: '#f5f5f5ab', };
+    const headerRowStyle = { backgroundColor: '#deb5b545' };
     if (isHeader) {
-        if (textSecondCell) return (<tr><th>{textFirstCell}</th><th>{textSecondCell}</th></tr>);
-        return (<tr><th colSpan={2}>{textFirstCell}</th></tr>);
+        if (textSecondCell) return (<tr style={rowStyle}><th style={headerRowStyle}>{textFirstCell}</th><th style={headerRowStyle}>{textSecondCell}</th></tr>);
+        return (<tr style={rowStyle}><th style={headerRowStyle} colSpan={2}>{textFirstCell}</th></tr>);
     }
     return (
-        <tr>
+        <tr style={rowStyle}>
             <td>{textFirstCell}</td>
             <td>{textSecondCell}</td>
         </tr>
