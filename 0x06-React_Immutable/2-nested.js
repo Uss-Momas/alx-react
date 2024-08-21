5647fc10ab86b594e48d990c72d6710b71b1170f
ussumane <ussumane.momade@uem.ac.mz>
2024-08-21 19:01:35 +0200

    fix error

--- 0x06-React_Immutable/2-nested.js
+++ 0x06-React_Immutable/2-nested.js
@@ -1,6 +1,6 @@
-import { Map } from './node_modules/immutable/dist/immutable';
+import { getIn, fromJS } from './node_modules/immutable/dist/immutable';
 
 export default function accessImmutableObject(object, array) {
-  const map = Map(object);
-  return map.getIn(array);
+  const map = fromJS(object);
+  return getIn(map, array);
 }
